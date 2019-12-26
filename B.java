bdada86b019786970d6d3ad77c823c64cd047d35
rish079 <rishiu.kumar@gmail.com>
2019-12-26 23:16:39 +0530

    my first commit

---
+++ B.java
@@ -0,0 +1,12 @@
+package pack2;
+import pack1.A;
+class B extends A{
+	public static void main(String[] args){
+		//A a = new A();
+		//a.m1();
+		B b = new B();
+		b.m1();
+		//A a1 = new B();
+		//a1.m1();
+	}
+}
---
+++ C.java
@@ -0,0 +1,10 @@
+interface Interf{
+	public void m1();
+	public void m2();
+}
+abstract class ServiceProvide implements Interf{
+	public void m1(){
+		
+	}
+	 
+}
